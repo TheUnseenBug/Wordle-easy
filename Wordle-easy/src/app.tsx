e7288a6139f05e1ec6800c440ad52274dd1c8bd9
dennis granheimer <dennis.granheimer@live.se>
2025-04-14 15:15:02 +0200

    start of layout

--- Wordle-easy/src/app.tsx
+++ Wordle-easy/src/app.tsx
@@ -3,7 +3,18 @@ import "./app.css";
 export function App() {
   return (
     <>
-      <div>hello</div>
+      <header className="flex items-center justify-center">
+        <h1>Wordle</h1>
+      </header>
+      <main class="flex justify-center">
+        <section className="grid grid-cols-5 max-w-xl gap-4 justify-center items-center">
+          <div className="border-2 flex justify-center items-center ">x</div>
+          <div className="border-2 flex justify-center items-center ">x</div>
+          <div className="border-2 flex justify-center items-center ">x</div>
+          <div className="border-2 flex justify-center items-center ">x</div>
+          <div className="border-2 flex justify-center items-center ">x</div>
+        </section>
+      </main>
     </>
   );
 }
